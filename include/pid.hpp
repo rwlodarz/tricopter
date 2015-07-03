b506a9f7779c0a8545d2a4a67730228e465e303c
rwlodarz <rafalwlodarz@gmail.com>
2015-07-03 13:03:09 +0200

    Adding pid implementation

---
+++ include/pid.hpp
@@ -0,0 +1,55 @@
+/*
+ * pid.hpp
+ *
+ *  Created on: Jul 3, 2015
+ *      Author: rwlodarz
+ */
+
+#ifndef INCLUDE_PID_HPP_
+#define INCLUDE_PID_HPP_
+
+#include <vector>
+
+struct PidConfiguration
+{
+    float p;
+    float i;
+    float d;
+    float epsilon;
+    float miniumim;
+    float maximum;
+};
+
+class Pid
+{
+public:
+
+    Pid(PidConfiguration& p_pidConfiguration, float p_samplingTime);
+    ~Pid();
+    
+    void computeOutput();
+
+    void setConfiguration(const PidConfiguration& p_configuration) { m_configuration = p_configuration; }
+    void setProcessVariable(float p_processVariable);
+    void setSamplingTime(float p_samplingTime) { m_samplingTime = p_samplingTime; }
+    void setSetPoint(float p_setPoint) { m_setPoint = p_setPoint; }
+
+    const PidConfiguration& getPidConfiguration() const { return m_configuration; }
+    float getOutput() const { return m_output; }
+private:
+    inline void computeTypeA();
+    inline void computeTypeB();
+    inline void computeTypeC();
+
+    float m_setPoint;
+    float m_samplingTime;
+    float m_output;
+    float m_i;
+    std::vector<float> m_processVariable;
+    std::vector<float> m_error;
+    PidConfiguration m_configuration;
+};
+
+
+
+#endif /* INCLUDE_PID_HPP_ */
---
+++ src/pid.cpp
@@ -0,0 +1,73 @@
+/*
+ * pid.cpp
+ *
+ *  Created on: Jul 3, 2015
+ *      Author: rwlodarz
+ */
+
+#include "include/pid.hpp"
+
+#include <algorithm>
+
+Pid::Pid(PidConfiguration& p_pidConfiguration, float p_samplingTime) :
+            m_configuration(p_pidConfiguration),
+            m_samplingTime(p_samplingTime),
+            m_output(0.0),
+            m_processVariable{0.0, 0.0, 0.0},
+            m_setPoint(0.0),
+            m_i(0.0),
+            m_error{0.0, 0.0, 0.0}
+{
+
+}
+
+void Pid::computeOutput()
+{
+    m_i = 0;
+
+    m_error[2] = m_error[1];
+    m_error[1] = m_error[0];
+    m_error[0] = m_samplingTime - m_processVariable;
+
+    if(std::abs(m_error[0] < m_configuration.epsilon))
+        m_i = m_configuration.i * m_samplingTime * m_error[0];
+
+    if(m_configuration.epsilon > 0)
+    {
+        computeTypeC();
+    }
+    else
+    {
+         if(m_error[0] < -10)
+              m_output = m_configuration.miniumim;
+         else if(m_error[0] > 10)
+              m_output = m_configuration.maximum;
+    }
+
+    m_output = std::max(std::min(m_output,m_configuration.maximum),m_configuration.miniumim); //TODO: WTF?!
+}
+
+void Pid::setProcessVariable(float p_processVariable)
+{
+    m_processVariable[2] = m_processVariable[1];
+    m_processVariable[1] = m_processVariable[0];
+    m_processVariable[0] = p_processVariable;
+}
+
+void Pid::computeTypeA()
+{
+    //TODO: uzupelnic w razie potrzeby
+}
+
+void Pid::computeTypeB()
+{
+    //TODO: uzupelnic w razie potrzeby
+}
+
+void Pid::computeTypeC()
+{
+    m_output = m_output - m_configuration.p*(m_processVariable[0] - m_processVariable[1]) + m_i - m_configuration.d*(m_processVariable[0] - 2*m_processVariable[1] + m_processVariable[2]) / m_samplingTime;
+}
+
+
+
